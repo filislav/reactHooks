57ecc5feed6aa615631f2aad3326baea6ba5d970
filislav <filislav43@gmail.com>
2021-06-04 11:45:16 +0400

    useState,useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React, { Component } from 'react'
-import TodoList from './TodoList'
+import React,{useState,useEffect} from 'react';
+import TodoList from './TodoList';
 
-export default class App extends Component {
-  state = {
-    todos: [
-      {id: 1, title: 'First todo', completed: false},
-      {id: 2, title: 'Second todo', completed: true},
-    ]
+const App =()=> {
+  const [todos,setTodos] = useState([]);
+  const[todoTitle,setTodoTitle] = useState('');
+
+const handleClick = ()=>{
+  console.log("click");
+}
+
+useEffect(()=>{
+  const raw = localStorage.getItem('todos')||[];
+  setTodos(JSON.parse(raw));
+},[]);
+
+useEffect(()=>{
+  document.addEventListener("click",handleClick);
+  localStorage.setItem('todos',JSON.stringify(todos));
+  return () =>{
+    document.removeEventListener("click",handleClick);
   }
-
-  render() {
+},[todos]);
+  const addTodo =(event)=>{
+    if(event.key === "Enter"){
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title:todoTitle,
+          completed:false  
+        }        
+      ]);
+      setTodoTitle('');
+    }
+  }
     return (
       <div className="container">
         <h1>Todo app</h1>
-
           <div className="input-field">
-            <input type="text" />
+            <input type="text" value={todoTitle} 
+            onChange={event=>setTodoTitle(event.target.value)}
+            onKeyPress={addTodo}/>
             <label>Todo name</label>
           </div>
-
-          <TodoList todos={this.state.todos} />
+          <TodoList todos={todos}/>
       </div>
     );
-  }
 }
+export default App;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,21 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react';
 
-export default function TodoItem({title, id, completed}) {
+const TodoItem=({title, id, completed})=> {
+  const [checked,setChecked] = useState(completed);
+
+  const cls =['todo'];
+  if(checked){
+    cls.push('completed');
+  }
   return (
-    <li className="todo">
+    <li className={cls.join(' ')}>
       <label>
-        <input
-          type="checkbox"
-          defaultChecked={false}
-        />
+        <input type="checkbox" checked={checked} 
+          onChange={()=>setChecked(!checked)}/>
         <span>{title}</span>
-
-        <i
-          className="material-icons red-text"
-        >
+        <i className="material-icons red-text">
           delete
         </i>
       </label>
     </li>
   )
 }
+export default TodoItem;
